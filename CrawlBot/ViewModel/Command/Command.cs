dbab2942cd30bf839fdcae30669b70ad96d8aeac
B0WL <kjiforouter@gmail.com>
2020-08-23 19:12:17 +0900

    Init project

---
+++ CrawlBot/ViewModel/Command/Command.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CrawlBot.ViewModel.Command
+{
+    public class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        public Action Action;
+
+        public Command(Action action)
+        {
+            Action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Action.Invoke();
+        }
+    }
+}
